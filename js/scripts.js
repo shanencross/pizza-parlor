7678c9b405ab6b3d921841f0424a2caf8feb1693
Shanen Cross <shanencross@yahoo.com>
2021-03-02 15:32:41 -0800

    Fills in calculatePrice, but doesn't pass test because price is being returned as a string, not a number

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,22 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.calculatePrice = function() {
-	return 0;
+	const basePrices = {
+		"small": 13.99,
+		"medium": 17.99,
+		"large": 20.99,
+		"extra-large": 23.99
+	}
+
+	const toppingPrices = {
+		"small": 2.50,
+		"medium": 2.75,
+		"large": 3.00,
+		"extra-large": 3.25
+	}
+
+	const basePrice = basePrices[this.size];
+	const toppingPrice = this.toppings.length * toppingPrices[this.size];
+	const price = (basePrice + toppingPrice).toFixed(2);
+	return price;
 }
