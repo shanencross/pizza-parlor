2cdd4689b6f68c447a41fd9f062184c426939d72
Shanen Cross <shanencross@yahoo.com>
2021-03-02 22:01:05 -0800

    Display resulting price

--- js/index.js
+++ js/index.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
 		event.preventDefault();
 
 		const size = $("#order-form #pizza-size").val();
+		
 		const toppingElements = $("#order-form input:checkbox[name=toppings]:checked");
-
 		let toppings = [];
 		for (toppingElement of toppingElements) {
 			toppings.push(toppingElement.value);
 		}
+
+		let pizza = new Pizza(size, toppings);
+
+		$("#order-confirmation").show();
+		$("#order-confirmation #pizza-price").text("$" + pizza.price);
 	})
 })
