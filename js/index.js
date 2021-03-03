463dc6990cdbb411926e81dca3e8b70475e42400
Shanen Cross <shanencross@yahoo.com>
2021-03-02 23:03:12 -0800

    Refactor to better organize code

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$("#order-form").submit(function(event) {
 		event.preventDefault();
 
+		// Get and store user input for pizza size and toppings
 		const size = $("#order-form #pizza-size").val();
-		
 		const toppingElements = $("#order-form input:checkbox[name=toppings]:checked");
 		let toppings = [];
 		for (toppingElement of toppingElements) {
@@ -12,14 +12,22 @@ $(document).ready(function() {
 
 		let pizza = new Pizza(size, toppings);
 
-		$("#order-placement").hide();
-		$("#order-confirmation").show();
-		$("#order-confirmation #pizza-price").text("$" + pizza.price);
+		displayOrderConfirmation(pizza);
 	})
 
-	$("button#new-order").click(function(event) {
-		$("#order-placement").show();
-		$("#order-confirmation").hide();
-		$("#order-form")[0].reset();
-	})
+	$("button#new-order").click(resetForNewOrder);
 })
+
+function displayOrderConfirmation(pizza) {
+	$("#intro").hide();
+	$("#order-placement").hide();
+	$("#order-confirmation").show();
+	$("#order-confirmation #pizza-price").text("$" + pizza.price);
+}
+
+function resetForNewOrder() {
+	$("#intro").show();
+	$("#order-placement").show();
+	$("#order-confirmation").hide();
+	$("#order-form")[0].reset();
+}
