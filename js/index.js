d4cb9ccce2f39b8c32c0095c881a09c821c716a3
Shanen Cross <shanencross@yahoo.com>
2021-03-02 21:52:10 -0800

    Get and store user inputs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-	
+	$("#order-form").submit(function(event) {
+		event.preventDefault();
+
+		const size = $("#order-form #pizza-size").val();
+		const toppingElements = $("#order-form input:checkbox[name=toppings]:checked");
+
+		let toppings = [];
+		for (toppingElement of toppingElements) {
+			toppings.push(toppingElement.value);
+		}
+	})
 })
